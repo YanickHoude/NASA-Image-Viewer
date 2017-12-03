23cc6461e76c621e50d99ca4d98d20bf662cc7e9
Yanick Houde <yhoude@uwo.ca>
2017-12-03 02:00:02 +0000

    search working, first image extracted

--- nasa-project/nasa-app/src/app/components/search/search.component.ts
+++ nasa-project/nasa-app/src/app/components/search/search.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
+import {Observable} from 'rxjs/Observable'
+// Use this yanick you primitive cave man
+import { NgModel } from '@angular/forms';
+
+// Bring in the HTTP module 
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+// Import data service to let us make HTTP calls 
+import {DataService} from '../../data.service';
+
 
 //allow the use of jQuery
 declare var jquery:any;
@@ -10,17 +20,80 @@ declare var $:any;
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class SearchComponent implements OnInit {
 
   router:Router;
   authService:AuthService;
 
-  constructor(router:Router, _authService: AuthService) {
-      this.router = router;
-      this.authService = _authService;
-  };
+  searchText:string = '';
+  nasaCollection:any[] = new Array();
+  nasaImages: any[] = new Array();
+  nasaObjects: any;
+  
+ constructor(router:Router, _authService: AuthService, private cdRef: ChangeDetectorRef, private _dataservice: DataService, private http:Http) {
+   
+   this.router = router;
+   this.authService = _authService;
+   
+ };
+
+  // Display the current value of the string in the console log
+  searchDatabase(){
+    
+    // this.cdRef.detectChanges();
+    
+    console.log("The value of the variable: " + this.searchText)
+    
+   //creating an object that holds what the user searched
+    var postObj: { property: string; } = { property: this.searchText };
+    
+    var me = this;
+    
+    var url = 'https://images-api.nasa.gov/search?q=' + this.searchText;
+    
+    console.log(this.searchText);
+    
+
+      
+    $.getJSON(url, function(data){
+      
+      //console.log(JSON.stringify(data));
+      
+      for(let coll of data.collection.items){
+        
+      var us = me;
+      
+        //console.log(coll.href);
+  
+        $.getJSON(coll.href, function(result){
+          
+            console.log("picture link   " + result[0]);
+          
+            //console.log(result);
+            
+            us.nasaImages = [];
+          
+            $.each(result, function(i, ilink){
+              
+             // console.log(ilink);
+              us.nasaImages.push(ilink);
+              
+          });
+          
+          console.log(us.nasaImages[0]);
+          
+          us.nasaCollection.push(us.nasaImages);
+          
+          us.cdRef.detectChanges();
+          });
+      }
+        });
+
+  } 
   
 
   ngOnInit() {
--- nasa-project/nasa-app/src/app/components/searchbar/searchbar.component.ts
+++ nasa-project/nasa-app/src/app/components/searchbar/searchbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import {Observable} from 'rxjs/Observable'
 // Use this yanick you primitive cave man
 import { NgModel } from '@angular/forms';
 
@@ -18,7 +18,8 @@ declare var $:any;
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.css']
+  styleUrls: ['./searchbar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class SearchbarComponent  {
@@ -27,18 +28,11 @@ export class SearchbarComponent  {
   searchText:string = '';
   nasaObjects: any[];
   
-  //trying this out
-  titles: any[];
-  collections: any[];
-  
-  
-  constructor(private _dataservice: DataService, private http:Http) {
-    
-
-  };
+ constructor(private ref: ChangeDetectorRef, private _dataservice: DataService, private http:Http) {};
 
   // Display the current value of the string in the console log
   searchDB(){
+    this.ref.detectChanges();
     
     console.log("The value of the variable: " + this.searchText)
     
@@ -46,65 +40,23 @@ export class SearchbarComponent  {
     var postObj: { property: string; } = { property: this.searchText };
     
     // return as type json
-    this._dataservice.searchImages(postObj).subscribe( function(res){
-    
-      //empty array that will hold all collections
-      var allcolls = [];
+    this._dataservice.searchImages(postObj).subscribe(async res => {
+
        
       console.log("The amount of items: " + (JSON.parse(res["_body"]).collection.items.length ))
-        
-    
-      for(let object of (JSON.parse(res["_body"]).collection.items)){
-          
-        //this is a literal object that will hold all the info we need about the collections
-        var Coll: {title: any, coll: any[] } = {title:'', coll:[]}
-            
-        //assigning the collection title
-        Coll.title = object.data[0].title;
-        
-        //check me out
-        var index = 0;
-        var test = [];
-        
-        //object.href actually gives you a URL to a JSON file, so this is just to get the actual JSON
-        $.getJSON(object.href, function(result){
-            
-            //the JSON file contains an array of image url's so we iterate through and add them to the array in our Coll object
-            $.each(result, function(i, ilink){
-                
-                //getfacked - here's where I'm getting bent over
-                Coll.coll.push(ilink);//--> this little doo dad over here adds an undefined element to the array and does fuck all
-                console.log(ilink);//--> this litte doo dad over here prints out the idividual fucking image links no problemo
-                //basically "ilink" is what it is supposed to be, but something about appending it to an array makes it worthless
-                //already tried: declaring a basic ass array (not part of an object) same issue
-                //already tried: assigin using "Coll.coll[index] = ilink" 
-                //already treid: stringifying that bitch, and parsing that bitch
-                
-                index = index + 1;
-            });
-        });
-        
-        console.log(Coll.coll);
-        allcolls.push(Coll);
-        
-        //test = this.http.get(object.href).map(res => JSON.stringify(res.json));
-        
-        // for(let img of object.href){
-        //     collectionstemp[colindex][imgindex] = img;
-        //     imgindex = imgindex + 1;
-        //     console.log(img);
-        // }
-        
-      };
+
+      console.log('The items: ' + JSON.stringify((JSON.parse(res["_body"]).collection.items)))
+
+
+
+        this.nasaObjects = (JSON.parse(res["_body"]).collection.items)
+        this.ref.detectChanges();
+  
       
-      //this.titles = titlestemp;
-      this.collections = allcolls;
+      }
+    )
       
-      console.log("first collection: ", this.collections[0]);
-      console.log("second collections: ", this.collections[1]);
-    })
-    
-  }
+  } 
   
 }
 
